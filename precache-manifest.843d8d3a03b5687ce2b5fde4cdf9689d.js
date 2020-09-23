9e96c0d6a1c09bf54d7082e5b6bf1868c30e1111
klimbachiy <klimbachiy@wiley.com>
2020-09-23 14:17:11 +0530

    Updates

---
+++ precache-manifest.843d8d3a03b5687ce2b5fde4cdf9689d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b7543d40834e37cfd807c70b3b87628",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "733a5258bf905949e71b",
+    "url": "/todoapp/static/css/main.7aef6805.chunk.css"
+  },
+  {
+    "revision": "6acfb2807769e33b667a",
+    "url": "/todoapp/static/js/2.5abca27f.chunk.js"
+  },
+  {
+    "revision": "e8b0ae1bd2f59a85f300b3c60c50941a",
+    "url": "/todoapp/static/js/2.5abca27f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "733a5258bf905949e71b",
+    "url": "/todoapp/static/js/main.5e172bbf.chunk.js"
+  },
+  {
+    "revision": "80b05f8f46ecf0bf2c2b",
+    "url": "/todoapp/static/js/runtime-main.5545d9fc.js"
+  }
+]);

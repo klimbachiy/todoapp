cfbab54f6c93892807a31ad35b3c41ad174725b1
klimbachiy <klimbachiy@wiley.com>
2020-09-30 16:01:52 +0530

    added toast msgs

--- src/Todo.js
+++ src/Todo.js
@@ -3,27 +3,41 @@ import './App.css'
 import Todoui from './Todoui';
 import Addtodo from './Addtodo';
 import uuid from 'react-native-uuid';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+
+toast.configure();
 
 class Todo extends React.Component{
     state={
         todos:[{id:1,name:"make bed"},
                 {id:2,name:"brush ur teeth"}]
     }
+    notifyError= () => toast.error("Oops invalid TODO!",{position:toast.POSITION.TOP_LEFT});
+    notifySuccess = () => toast.success("TODO Added",{position:toast.POSITION.TOP_LEFT});
+    notify = () => toast.info("TODO Deleted!",{position: toast.POSITION.TOP_LEFT})
     deleteTodo=(id)=>{
-        //console.log(id);
         let todos = this.state.todos.filter(todo=>{
             return todo.id!==id
         })
         this.setState({
             todos:todos
         });
+        this.notify();
     }
     addTodo=(todo)=>{
-        todo.id=uuid.v4();
-        let todos=[...this.state.todos,todo];
-        this.setState({
-            todos:todos
-        })
+        if(todo.name===""){
+            this.notifyError();
+        }
+        else{
+            todo.id=uuid.v4();
+            let todos=[...this.state.todos,todo];
+            this.setState({
+                todos:todos
+            })
+            this.notifySuccess();
+        }
     }
     render(){
         return(
